4469568e32cd86856502aa620b19d79b0db08e3e
DmitryYaroslavtsev <yaroslav-demon@yandex.ru>
2017-12-25 12:22:57 +0300

    testDB

--- src/begin/test.java
+++ src/begin/test.java
@@ -13,7 +13,7 @@ public class test {
 
     private void testDatabase() {
         try {
-            Class.forName("org.postgresql.Driver");
+            //Class.forName("org.postgresql.Driver");
             String url = "jdbc:postgresql://localhost:5432/contactdb";
             String login = "postgres";
             String password = "postgres";
